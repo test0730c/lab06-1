918abf8085adda4dd5452e17f1b5e19bf2d796bb
Sveta1305 <satim2502@mail.ru>
2022-04-04 18:40:50 +0300

    Changed all  files

---
+++ Solver/_build/CMakeFiles/formatter_ex_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/formatter_ex_lib.dir/home/lis/Sveta1305/workspace/projects/lab03/homework_tmp/formatter_ex_lib/formatter_ex.cpp.o"
+  "libformatter_ex_lib.a"
+  "libformatter_ex_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/formatter_ex_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
