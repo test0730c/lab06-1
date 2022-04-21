6189b7cf0523dc5806ca6412dbacd59a49e7e469
Sveta1305 <satim2502@mail.ru>
2022-04-21 19:13:42 +0300

    Added files[3]

--- _build/cmake_install.cmake
+++ _build/cmake_install.cmake
@@ -38,30 +38,25 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/home/lis/Sveta1305/workspace/projects/lab06/_build/libprint.a")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/solver_application" TYPE DIRECTORY FILES "/home/lis/Sveta1305/workspace/projects/lab06/solver_application/")
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE DIRECTORY FILES "/home/lis/Sveta1305/workspace/projects/lab06/include/")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/solver_lib" TYPE DIRECTORY FILES "/home/lis/Sveta1305/workspace/projects/lab06/solver_lib/")
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/cmake/print-config.cmake")
-    file(DIFFERENT EXPORT_FILE_CHANGED FILES
-         "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/cmake/print-config.cmake"
-         "/home/lis/Sveta1305/workspace/projects/lab06/_build/CMakeFiles/Export/cmake/print-config.cmake")
-    if(EXPORT_FILE_CHANGED)
-      file(GLOB OLD_CONFIG_FILES "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/cmake/print-config-*.cmake")
-      if(OLD_CONFIG_FILES)
-        message(STATUS "Old export file \"$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/cmake/print-config.cmake\" will be replaced.  Removing files [${OLD_CONFIG_FILES}].")
-        file(REMOVE ${OLD_CONFIG_FILES})
-      endif()
-    endif()
-  endif()
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/cmake" TYPE FILE FILES "/home/lis/Sveta1305/workspace/projects/lab06/_build/CMakeFiles/Export/cmake/print-config.cmake")
-  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^()$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/cmake" TYPE FILE FILES "/home/lis/Sveta1305/workspace/projects/lab06/_build/CMakeFiles/Export/cmake/print-config-noconfig.cmake")
-  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/formatter_ex_lib" TYPE DIRECTORY FILES "/home/lis/Sveta1305/workspace/projects/lab06/formatter_ex_lib/")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/formatter_lib" TYPE DIRECTORY FILES "/home/lis/Sveta1305/workspace/projects/lab06/formatter_lib/")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/home/lis/Sveta1305/workspace/projects/lab06/_build/formatter_ex_lib/cmake_install.cmake")
+
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
--- solver_application/README.md
+++
@@ -1,1 +0,0 @@
-[![Coverage Status](https://coveralls.io/repos/github/Sveta1305/lab06/badge.svg?branch=main)](https://coveralls.io/github/Sveta1305/lab06?branch=main)
